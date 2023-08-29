3c87db6469af1880a993778415e71a9e0d1d1a9b
George Dudaev <georgeduday@gmail.com>
2023-08-29 16:42:18 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # mq-app
+
+Start server (sender):
+
+go run cmd/server/server.go
+
+Start client (reciever):
+
+go run cmd/client/client.go
---
+++ cmd/server/server.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // autodelete
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // args
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Recieved a message: %s", d.Body)
+		}
+	}()
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
